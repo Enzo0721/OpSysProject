818548300883d0365185115e21ff413e55f52b11
Thomas Dou <thomasyd1120@gmail.com>
2024-07-23 01:22:32 -0400

    changed print formatting, changed process to a struct, made generateBursts function prototype

--- process.h
+++ process.h
@@ -1,15 +1,16 @@
+#ifndef PRCS_H
+#define PRCS_H
+
 #include <iostream>
 #include <vector>
 #include <string>
 
-class Process {
-public:
+struct Process {
     std::string pid;
     int arrival_time;
     bool cpuBound;
     std::vector<int> cpu_bursts;
     std::vector<int> io_bursts;
-
-    /*Process(const std::string& pid, int arrival, bool cpuBound)
-        : pid(pid), arrival_time(arrival), cpuBound(cpuBound) {}*/
 };
+
+#endif
