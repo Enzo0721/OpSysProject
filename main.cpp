ca3b2665ca0414fc52df7ab4a66b7fb86059330c
Enzo0721 <kayajergin@gmail.com>
2024-07-23 21:58:58 -0400

    fixed process generation with correct arrival times, burst amounts, cpu burst times, and io burst times

--- main.cpp
+++ main.cpp
@@ -4,7 +4,15 @@
 #include <iomanip>
 #include <cmath>
 #include <random>
-#include "process.h"
+
+
+struct Process {
+    std::string pid;
+    int arrival_time;
+    bool cpuBound;
+    std::vector<int> cpu_bursts;
+    std::vector<int> io_bursts;
+};
 
 double next_exp(double lambda, double upper_bound) {
     double exp_val;
@@ -18,28 +26,44 @@ double next_exp(double lambda, double upper_bound) {
     return exp_val;
 }
 
-//params that are ints here might be used to track total CPU burst time
-void generateBursts(Process currentProcess, int & CPU_total, int & IO_Total){
-    return;
-}
-
-std::vector<Process>  generateProcess(int n, int ncpu, double lambda, double upper_bound) {
-    std::vector <Process> processes;
+std::vector<Process> generateProcesses(int n, int ncpu, double lambda, double upper_bound) {
+    std::vector<Process> processes;
 
     for (int i = 0; i < n; i++) {
         Process p;
-        p.pid = std::string(1, 'A' + i / 10) + std::to_string(i % 10);
         p.cpuBound = i < ncpu;
         p.arrival_time = std::floor(next_exp(lambda, upper_bound));
-        //std::cout << "PROCESS ID: " << p.pid << std::endl;
-        //std::cout << "PROCESS ARRIVAL: " << p.arrival_time << std::endl;
+        p.pid = std::string(1, 'A' + (i / 10)) + std::to_string(i % 10);
+
         int burstNum = std::ceil(drand48() * 32);
-        //std::cout << "PROCESS BURST AMOUNT: " << burstNum << std::endl;
-        std::cout << "I/O-bound process " << p.pid <<": arrival time "<< p.arrival_time <<"; "<< burstNum <<" CPU bursts:" << std::endl;
+        std::cout << (p.cpuBound ? "CPU-bound" : "I/O-bound") << " process " << p.pid << ": arrival time " << p.arrival_time << "ms; " << burstNum << " CPU bursts:" << std::endl;
+
+        for (int j = 0; j < burstNum; ++j) {
+            int cpu_burst = std::ceil(next_exp(lambda, upper_bound));
+
+            if (p.cpuBound) {
+                cpu_burst *= 4;
+            }
+
+            p.cpu_bursts.push_back(cpu_burst);
+            std::cout << "==> CPU burst " << cpu_burst << "ms";
+
+            if (j < burstNum - 1) {  // don't make an io burst for last cpu burst
+                int io_burst = std::ceil(next_exp(lambda, upper_bound)) * 8;
+                
+                if (p.cpuBound) {
+                    io_burst /= 8;
+                }
+
+                p.io_bursts.push_back(io_burst);
+                std::cout << " ==> I/O burst " << io_burst << "ms";
+            }
+            std::cout << std::endl;
+        }
         processes.push_back(p);
-    } 
+    }
     return processes;
-}   
+} 
 
 int main(int argc, char** argv) {
     if (argc != 6) {
@@ -64,7 +88,7 @@ int main(int argc, char** argv) {
     std::cout << "<<< -- process set(n=" << n << ") with " << ncpu << " CPU-bound process" << (ncpu == 1 ? "" : "es") << std::endl;
     std::cout << "<<< -- seed=" << seed << "; lambda=" << std::fixed << std::setprecision(6) << lambda << "; bound=" << std::setprecision(0) << upper_bound << std::endl;
 
-    std::vector<Process> Processes = generateProcess(n, ncpu, lambda, upper_bound);
+    std::vector<Process> Processes = generateProcesses(n, ncpu, lambda, upper_bound);
 
     return 0;
 }
