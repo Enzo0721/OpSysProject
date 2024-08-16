107881f7b5e7e119c8ef9b4b072a9a89cdc77355
tdou25 <thomasyd1120@gmail.com>
2024-08-16 01:51:10 -0400

    began FCFS implementation

--- main.cpp
+++ main.cpp
@@ -6,10 +6,13 @@
 #include <cmath>
 #include <random>
 #include <numeric>
+#include <list>
+#include <algorithm>
 
 struct Process {
     std::string pid;
     int arrival_time;
+    int queue_entry_time = -1;
     bool cpuBound;
     std::vector<int> cpu_bursts;
     std::vector<int> io_bursts;
@@ -106,9 +109,58 @@ void generateStatistics(const std::vector<Process>& processes, int n, int nCpu) 
     outFile.close();
 }
 
+bool compare_by_arrival_time(const Process & left, const Process & right){
+    return left.arrival_time < right.arrival_time;
+}
+
+void remove_latest(std::list<Process> & start, std::list<Process> & end){
+    std::cout << "hello world" << std::endl;
+}
+
+void move_process(int tcs, std::list<Process> start, std::list<Process> destination){
+    int moving_time = (int) tcs/2;
+    Process temp = start.front();
+    start.pop_front();
+
+    temp.arrival_time += moving_time;
+    
+    destination.push_back(temp);
+    std::sort(destination.begin(), destination.end(), compare_by_arrival_time);
+}
+
+void run_FCFS(std::vector<Process> & Processes){
+    int tick = 0;
+    std::list<Process> unarrived_processes;
+    std::copy(Processes.begin(), Processes.end(), std::back_inserter(unarrived_processes));
+    std::sort(Processes.begin(), Processes.end(), compare_by_arrival_time);
+
+    int num_processes = Processes.size();
+
+    std::list<Process> queue;
+    std::list<Process> blocking_on_io;
+    std::list<Process> moving_to_completed;
+    std::list<Process> completed;
+    // for(const auto & p : unarrived_processes){
+    //     std::cout << p.pid << " " << p.arrival_time << std::endl;
+    // }
+    // std::cout << (*unarrived_processes.begin()).cpu_bursts.size() << std::endl;
+    std::cout << *((*unarrived_processes.begin()).cpu_bursts.begin()) << std::endl;
+    std::cout << *((*unarrived_processes.begin()).io_bursts.begin()) << std::endl;
+
+    while(tick < 10000){
+        /*checks:
+        -Check for newly arrived processes
+        -Check for processes blocked on io
+        -Check in queue for next process to begin CPU burst
+        -Check for processes that need to be moved from moving_to_completed to completed
+        */
+    
+    }
+}
+
 int main(int argc, char** argv) {
-    if (argc != 6) {
-        std::cerr << "ERROR: Enter 5 command line arguments\n" << std::endl;
+    if (argc != 9) {
+        std::cerr << "ERROR: Enter 7 command line arguments\n" << std::endl;
         return 1;
     }
     int n = std::stoi(argv[1]); /* Number of processes to simulate */
@@ -116,7 +168,12 @@ int main(int argc, char** argv) {
     int seed = std::stoi(argv[3]); /* seed for pseudo-random number sequence */
     double lambda = std::stod(argv[4]); /* lambda*/
     double upper_bound = std::stod(argv[5]); /* random value upper bound */
-    if (n <= 0 || n > 260 || ncpu < 0 || ncpu > n || lambda <= 0 || upper_bound <= 0) {
+    int tcs = std::stoi(argv[6]); /* context switch time */
+    float alpha = std::stof(argv[7]); /* true arrival time constant */
+    int tslice = std::stoi(argv[8]); /* round robin time slice */
+
+    if (n <= 0 || n > 260 || ncpu < 0 || ncpu > n || lambda <= 0 || upper_bound <= 0 
+    || tcs <= 0 || (tcs % 2) != 0 || alpha > 1.0 || alpha < 0.0 || tslice <= 0) {
         std::cerr << "ERROR: Invalid input parameters\n" << std::endl;
         return 1;
     }
@@ -126,5 +183,9 @@ int main(int argc, char** argv) {
     std::cout << "<<< -- seed=" << seed << "; lambda=" << std::fixed << std::setprecision(6) << lambda << "; bound=" << std::setprecision(0) << upper_bound << std::endl;
     std::vector<Process> Processes = generateProcesses(n, ncpu, lambda, upper_bound);
     generateStatistics(Processes, n, ncpu);
+
+    std::cout << "<<< PROJECT PART II"<< std::endl;
+    std::cout << "<<< -- t_cs="<< tcs << "ms; alpha="<< std::fixed << std::setprecision(2) << alpha << "; t_slice=" << tslice <<"ms" << std::endl;
+    run_FCFS(Processes);
     return 0;
 }
